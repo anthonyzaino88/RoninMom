a9658bf7bd072137fcd05db380862e5f4a5d70d7
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-27 19:59:06 +0000

    maybe workls

--- public/src/signup.js
+++ public/src/signup.js
@@ -18,5 +18,18 @@ export function signUpUser(email, password) {
 }
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  const signupForm = document.getElementById('signup-form');
+  
+  if (signupForm) {
+    signupForm.addEventListener('submit', function(event) {
+      event.preventDefault();
 
+      const email = document.getElementById('signup-email').value;
+      const password = document.getElementById('signup-password').value;
+
+      signUpUser(email, password);
+    });
+  }
+});
 
