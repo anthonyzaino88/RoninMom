8feb62f84408ab24f63dfb656caf5b3a8c59c08f
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-28 16:57:36 +0000

    firestore - username

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
     signupForm.addEventListener('submit', function(event) {
       event.preventDefault();
 
+      const username = document.getElementById('signup-username').value; // Assuming you have an input field for username
       const email = document.getElementById('signup-email').value;
       const password = document.getElementById('signup-password').value;
 
-      signUpUser(email, password);
+      signUpUser(email, password, username);
     });
   }
 });
--- src/signup.js
+++ src/signup.js
@@ -1,18 +1,27 @@
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth';
+import { app } from './firebase-config';
 
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { app } from './firebase-config'; 
-
-export function signUpUser(email, password) {
+export function signUpUser(email, password, username) {
   const auth = getAuth(app);
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("User created: ", user);
+      // Update the user's profile with their username
+      return updateProfile(userCredential.user, {
+        displayName: username
+      });
+    })
+    .then(() => {
+      // Send an email verification
+      const user = auth.currentUser;
+      return sendEmailVerification(user);
+    })
+    .then(() => {
+      console.log("Verification email sent");
+      // Additional actions after sending verification email
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error("Error in sign up: ", errorMessage);
+      console.error("Error in sign up: ", error.message);
+      // Handle errors here
     });
 }
