309c86715e28faaaebdf51f5baf0042fa4daaca0
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-28 21:32:57 +0000

    create user

--- src/signup.js
+++ src/signup.js
@@ -1,47 +1,29 @@
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 import { app, db } from './firebase-config';
 
 export async function signUpUser(email, password, username) {
   const auth = getAuth(app);
 
   try {
-    console.log("Starting sign-up process for:", email);
-
-    // Check for username uniqueness
-    console.log("Checking if username exists:", username);
-    const usernameRef = doc(db, 'usernames', username);
-    const usernameSnap = await getDoc(usernameRef);
-    console.log("Username existence check completed");
-    
-    if (usernameSnap.exists()) {
-      console.log("Username already taken:", username);
-      throw new Error('Username is already taken');
-    }
-    console.log("Username is available:", username);
-
-    // Create user account
-    console.log("Creating user account");
+    console.log("Creating user account with email:", email);
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    console.log("User account created:", userCredential.user.uid);
+    const uid = userCredential.user.uid;
+    console.log("User account created with UID:", uid);
 
-    // Update user's profile
     console.log("Updating user's profile with display name:", username);
     await updateProfile(userCredential.user, { displayName: username });
     console.log("User profile updated");
 
-    // Store username in Firestore
-    console.log("Attempting to store username in Firestore", username);
-    await setDoc(doc(db, 'usernames', username), { uid: userCredential.user.uid });
-    console.log("Username stored in Firestore:", username);
+    console.log("Storing user data in Firestore under 'Users' collection");
+    await setDoc(doc(db, 'Users', uid), { username: username, email: email });
+    console.log("User data stored in Firestore:", uid);
 
-    // Send email verification
-    console.log("Attempting to send email verification");
+    console.log("Sending email verification");
     await sendEmailVerification(userCredential.user);
     console.log("Verification email sent");
 
   } catch (error) {
     console.error("Error during sign-up process:", error);
-    console.error("Error details:", error.message);
   }
 }
