d76f4ef4e9e06e39f04e8f4b5eb3bb65265a7f1e
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-29 17:50:17 +0000

    fixed

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,12 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addAdminRole = onCall(async (data, context) => {
-    const email = data.email;
-    try {
-        const user = await admin.auth().getUserByEmail(email);
-        await admin.auth().setCustomUserClaims(user.uid, { admin: true });
-        return { message: `Success! ${email} has been made an admin.` };
-    } catch (err) {
-        return { error: err.message };
-    }
+  const email = data.email;
+  try {
+    const user = await admin.auth().getUserByEmail(email);
+    await admin.auth().setCustomUserClaims(user.uid, {admin: true});
+    return {message: `Success! ${email} has been made an admin.`};
+  } catch (err) {
+    return {error: err.message};
+  }
 });
