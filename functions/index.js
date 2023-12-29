4577bf1b4ec3f3d320079415762c989172685fc9
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-29 16:41:39 +0000

    some new stuff

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    const email = data.email;
+
+    return admin.auth().getUserByEmail(email).then((user) => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true,
+        });
+    }).then(() => {
+        return { message: `Success! ${email} has been made an admin` };
+    }).catch((err) => {
+        return err;
+    });
+});
