eada2829743d796f3dddfb8f97971c2e909fb355
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-29 17:19:43 +0000

    fix maybe

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,16 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.addAdminRole = functions.https.onCall((data, context) => {
+exports.addAdminRole = functions.https.onCall(async (data, context) => {
   const email = data.email;
 
-  return admin.auth().getUserByEmail(email).then((user) => {
-    return admin.auth().setCustomUserClaims(user.uid, {
-      admin: true,
-    });
-  }).then(() => {
-    return {message: `Success! ${email} has been made an admin`};
-  }).catch((err) => {
-    return err;
-  });
+  try {
+        const user = await admin.auth().getUserByEmail(email);
+        await admin.auth().setCustomUserClaims(user.uid, {
+            admin: true,
+        });
+        return { message: `Success! ${email} has been made an admin` };
+    } catch (err) {
+        return err;
+    }
 });
