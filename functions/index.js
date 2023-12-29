93ae9caa563cc07f9f5bc20f06b6b91409ec1dce
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-12-29 17:48:37 +0000

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,14 @@
 const { onCall } = require("firebase-functions/v2/https");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addAdminRole = onCall(async (data, context) => {
-  // Ensure the caller is authorized to assign roles, typically by checking context.auth
-
-  const email = data.email;
-  try{
-    const user = await admin.auth().getUserByEmail(email);
-    await admin.auth().setCustomUserClaims(user.uid,{admin: true});
-    return{ message:`Success!${email}has been made an admin.`};
-   }catch(err){
-    return{error: err.message};
-  }
+    const email = data.email;
+    try {
+        const user = await admin.auth().getUserByEmail(email);
+        await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+        return { message: `Success! ${email} has been made an admin.` };
+    } catch (err) {
+        return { error: err.message };
+    }
 });
