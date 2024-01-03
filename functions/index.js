f2c32de34a2586b69bf77e1c08f1c2f7dcadfad5
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2024-01-03 01:38:24 +0000

    whoops

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,14 @@
-// Import Firebase configuration and necessary functions
-import './firebase-config';
-import { signInWithEmailAndPassword, getIdTokenResult } from 'firebase/auth';
-import { signUpUser } from './signup';
+const { onCall } = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// Function to setup the signup form listener
-function setupSignupForm() {
-  const signupForm = document.getElementById('signup-form');
-  if (signupForm) {
-    signupForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      const email = document.getElementById('signup-email').value;
-      const password = document.getElementById('signup-password').value;
-      const username = document.getElementById('signup-username').value;
-      signUpUser(email, password, username);
-    });
+exports.addAdminRole = onCall(async (data, context) => {
+  const email = data.email;
+  try {
+    const user = await admin.auth().getUserByEmail(email);
+    await admin.auth().setCustomUserClaims(user.uid, {admin: true});
+    return {message: `Success! ${email} has been made an admin.`};
+  } catch (err) {
+    return {error: err.message};
   }
-}
-
-// Function to setup the admin login form listener
-function setupAdminLoginForm() {
-  const adminLoginForm = document.getElementById('adminLoginForm');
-  if (adminLoginForm) {
-    adminLoginForm.addEventListener('submit', async function(event){
-      event.preventDefault();
-      const email = document.getElementById('adminEmail').value;
-      const password = document.getElementById('adminPassword').value;
-
-      try {
-          const userCredential = await signInWithEmailAndPassword(email, password);
-          const idTokenResult = await getIdTokenResult(userCredential.user);
-          if (idTokenResult.claims.admin) {
-              window.location.href = '/path-to-admin-dashboard.html';
-          } else {
-              console.error('Access denied. User is not an admin.');
-          }
-      } catch (error) {
-          console.error('Login failed:', error);
-      }
-    });
-  }
-}
-
-// Initialize the forms when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-  setupSignupForm();
-  setupAdminLoginForm();
 });
