ee58f0cc6752d0061e56188d453f637fd140db71
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2024-01-03 01:42:19 +0000

    admin

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-// Import Firebase configuration
+// Import Firebase configuration and necessary functions
 import './firebase-config';
-
-// Import your sign-up functionality
+import { signInWithEmailAndPassword, getIdTokenResult } from 'firebase/auth';
 import { signUpUser } from './signup';
 
-// Event listener for the form submission
-document.addEventListener('DOMContentLoaded', () => {
+// Function to setup the signup form listener
+function setupSignupForm() {
   const signupForm = document.getElementById('signup-form');
-  
   if (signupForm) {
     signupForm.addEventListener('submit', function(event) {
       event.preventDefault();
-    
       const email = document.getElementById('signup-email').value;
       const password = document.getElementById('signup-password').value;
-      const username = document.getElementById('signup-username').value; // Capture the username
-    
+      const username = document.getElementById('signup-username').value;
       signUpUser(email, password, username);
     });
   }
+}
+
+// Function to setup the admin login form listener
+function setupAdminLoginForm() {
+  const adminLoginForm = document.getElementById('adminLoginForm');
+  if (adminLoginForm) {
+    adminLoginForm.addEventListener('submit', async function(event){
+      event.preventDefault();
+      const email = document.getElementById('adminEmail').value;
+      const password = document.getElementById('adminPassword').value;
+
+      try {
+          const userCredential = await signInWithEmailAndPassword(email, password);
+          const idTokenResult = await getIdTokenResult(userCredential.user);
+          if (idTokenResult.claims.admin) {
+              window.location.href = '/path-to-admin-dashboard.html';
+          } else {
+              console.error('Access denied. User is not an admin.');
+          }
+      } catch (error) {
+          console.error('Login failed:', error);
+      }
+    });
+  }
+}
+
+// Initialize the forms when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  setupSignupForm();
+  setupAdminLoginForm();
 });
